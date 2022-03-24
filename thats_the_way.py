5b47ad58f753d7c9dbf304ca1825ac687a199f09
user <yardenge@edu.hac.ac.il>
2022-03-24 10:44:10 +0200

    I moved the logic into a function and changes the iterator

--- thats_the_way.py
+++ thats_the_way.py
@@ -1,13 +1,18 @@
 import os
 
-# input: path to a directory.
-# output: files names starts with "deep"
+"""
+ This functions receives a path from the user, then prints all the 
+ files names that starts with the word "deep"
+
+ @param path - string which is path to location in the computer
+"""
+
+
+def print_files(path):
+    dir_files_list = os.listdir(path)  # get a list of all files names in the wanted dir
+
+    print([file_name for file_name in dir_files_list if file_name.startswith("deep")])
+
 
 dir_path = input("Enter Directory path: ")  # take input from user
-
-dir_files_list = os.listdir(dir_path)  # get a list of all files names in the wanted dir
-
-# print the file name if it starts with "deep"
-for file_name in dir_files_list:
-    if file_name.startswith("deep"):
-        print(file_name)
+print_files(dir_path)
